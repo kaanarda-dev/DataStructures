363ceab6e257906f8f6dfda039d7f381735d9d2c
kaanarda-dev <kaan.arda205@gmail.com>
2026-09-04 17:16:12 +0300

    Implement binarySearchtree exercises in C Added addNode function and inorderTraversal function

---
+++ binarySearchTree.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+}node;
+
+
+struct Node *addNode(struct Node* root, int data) {
+
+    if (root == NULL) {
+
+        node* temp = (node*)malloc(sizeof(node));
+        temp->data = data;
+        temp->left = NULL;
+        temp->right = NULL;
+        return temp;
+
+    }
+
+    if (root->data < data) {
+
+        root->right = addNode(root->right, data);
+        return root;
+
+    }
+
+    root->left = addNode(root->left, data);
+    return root;
+
+
+
+
+}
+
+void inorderTraversal(struct Node *root) {
+
+    if (root == NULL) {
+        return;
+    }
+    inorderTraversal(root->left);
+    printf("%d ",root->data);
+    inorderTraversal(root->right);
+
+
+}
+
+
+
+int main() {
+
+
+    node *root = NULL;
+    root = addNode(root, 10);
+    root = addNode(root, 20);
+    root = addNode(root, 5);
+    root = addNode(root, 3);
+    root = addNode(root, 40);
+    inorderTraversal(root);
+
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+
+    printf("Welcome!\n");
+
     return 0;
 }
